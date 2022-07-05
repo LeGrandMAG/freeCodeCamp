345a12657d2ace8847d4edfdfa45de11d9bfe536
LeGrandMAG <64711820+LeGrandMAG@users.noreply.github.com>
2022-07-05 20:55:15 +0900

    Update DiffTwoArrays.js

--- Intermediate Algorithm Scripting/DiffTwoArrays.js
+++ Intermediate Algorithm Scripting/DiffTwoArrays.js
@@ -1,1 +1,10 @@
 /*This program compares two arrays and return a new array with any items only found in one of the two given arrays. */
+
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  arr1.filter( item => arr2.includes(item) === false).map(item => newArr.push(item))
+  arr2.filter( item => arr1.includes(item) === false).map(item => newArr.push(item))
+  return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
